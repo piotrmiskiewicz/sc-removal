1c87a22943f24fed8053766f0868aa2d40f188e1
Miskiewicz <piotr.miskiewicz@sap.com>
2021-07-29 12:54:51 +0200

    wip

--- hack/testing.sh
+++ hack/testing.sh
@@ -8,13 +8,15 @@ kubectl create ns kyma-system
 
 helm install service-catalog hack/charts/service-catalog --namespace kyma-system --set asyncBindingOperationsEnabled=true
 
-kubectl apply -f hack/charts/podpresets.settings.svcat.crd.yaml
 helm install pod-preset hack/charts/pod-preset --namespace kyma-system
 
 # helm broker
 helm install helm-broker hack/charts/helm-broker --namespace kyma-system
 
 kubectl apply -f hack/resources/sample-addons.yaml
+
+# wait for clusterserviceclasses
+kubectl get clusterserviceclass
 
 # create an instance and a binding
 kubectl apply -f hack/resources/instance.yaml
